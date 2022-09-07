5582920667b128fcb78f9449bbeeff1d1d741ae2
ruchitdhiman <87163293+ruchitdhiman@users.noreply.github.com>
2022-09-07 12:15:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import {useState} from "react";
+import List from './List';
+function App(){
+const [currentItem , setCurrentItem] = useState(null);
+const[itemList , updateItemList] = useState([]);
+const onChangeHandler = e =>{
+  setCurrentItem(e.target.value);
+  
+
+};
+const addItemToList=()=>{
+  updateItemList ([...itemList,{item : currentItem , key :Date.now()}]);
+  setCurrentItem("");
+
+};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" >
+        <h1> TO DO LIST</h1>
+        <div className="wrapper">
+          <div className="input-wrapper">
+          <input placeholder='add task' value={currentItem} onChange={onChangeHandler}/>
+          <button onClick={addItemToList}> +</button>
+          
+          
+          </div>
+          <List itemList = {itemList} updateItemList ={updateItemList}/>
+        </div>
+        
       </header>
     </div>
   );
---
+++ src/List.js
@@ -0,0 +1,32 @@
+import "./List.css";
+function List(props){
+    const deleteItem = key =>{
+
+    const newList = props.itemList.filter((itemObj)=>{
+       return itemObj.key!==key;
+
+    });
+    props.updateItemList(newList);
+
+};
+
+    return (
+    <div>
+        {props.itemList.map(itemObj =>{
+        
+            return(
+                <div key={itemObj.key} className="Item">
+            
+                 <p>{itemObj.item}</p> 
+                 <button onClick={() =>deleteItem(itemObj.key)}>x</button>
+                
+                 </div>
+
+            );
+        })}
+
+            
+    </div>
+    );
+}
+export default List;
